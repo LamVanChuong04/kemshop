485bc46b051e12110d5398e60245063ccfbbcde6
LamVanChuong04 <lamvanchuong04@gmail.com>
2025-08-29 18:10:16 +0700

    first commit

---
+++ src/main/java/com/bychuong/kemshop/KemshopApplication.java
@@ -0,0 +1,14 @@
+package com.bychuong.kemshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KemshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KemshopApplication.class, args);
+        System.out.println("Hello word");
+    }
+
+}
---
+++ src/test/java/com/bychuong/kemshop/KemshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bychuong.kemshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KemshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
